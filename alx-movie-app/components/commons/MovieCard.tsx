03f2870d588cdd9b2e1e992993f9be1bb746438b
Shyaka Ndamukunda <shyakandamukunda@gmail.com>
2025-08-12 14:24:43 +0200

    feat: initial Next.js project setup for alx-movie-app

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieCard: React.FC = () => {
+  return (
+    <div>MovieCard</div>
+  );
+};
+
+export default MovieCard;
---
+++ alx-movie-app/components/layouts/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Note: This is a duplicate file based on the instructions.
+// It's generally better to place reusable components in a single location like 'commons'.
+const Button: React.FC = () => {
+  return (
+    <button>Layout Button</button>
+  );
+};
+
+export default Button;
